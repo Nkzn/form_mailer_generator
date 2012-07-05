bbac38560f9761986aa7e2040b8b0a77bc443001
Nkzn <nkzn@nkzn.net>
2012-07-05 13:11:20 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sfmailer::Application.config.secret_token = 'e91367007325819b4acf1ed8a05823e3770d32fc891de7ba3b9776549e68703b83fcc6942c62e643d7f8c96ca55ddc7834c048c9559620ddb0263b27ae999235'
